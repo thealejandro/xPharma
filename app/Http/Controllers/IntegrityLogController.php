e767ff9cb13a99a768ec3b6b6e0c29e76634cd28
thealejandro <thealejandro7w7@gmail.com>
2025-04-18 13:31:32 -0600

    feat: Add controllers and models for various resources
    - Created PresentationController, ProductController, ProductPresentationController, PurchaseController, PurchaseItemController, PurchaseValidationController, SaleController, SaleDetailController, SaleReturnController, SaleReturnDetailController, ShipmentReturnController, StockMovementController, StockMovementDetailController, and SupplierController with basic CRUD methods.
    - Implemented models for Branch, CanceledInvoice, Client, IntegrityLog, InventoryLot, Invoice, Presentation, Product, ProductPresentation, Purchase, PurchaseItem, PurchaseValidation, Sale, SaleDetail, SaleReturn, SaleReturnDetail, ShipmentReturn, StockMovement, StockMovementDetail, and Supplier.
    - Added migration files for creating tables corresponding to the new models.

---
+++ app/Http/Controllers/IntegrityLogController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\IntegrityLog;
+use Illuminate\Http\Request;
+
+class IntegrityLogController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(IntegrityLog $integrityLog)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(IntegrityLog $integrityLog)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, IntegrityLog $integrityLog)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(IntegrityLog $integrityLog)
+    {
+        //
+    }
+}
